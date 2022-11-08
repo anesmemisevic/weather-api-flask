bd3b2dca4f161f38e1eb6bf2d7b9162e83ad82b0
Anes Memišević <anesmemisevic23@gmail.com>
2022-11-08 03:05:39 +0300

    Testing git account change

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def station(station, date):
-    temperature = 23
+    temperature = 25
     return {
         "station": station,
         "date": date,
