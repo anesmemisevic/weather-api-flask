c7e0797ff17c1c1527e04922ae66d5793d518f9a
Anes Memisevic <anesmemisevic23@gmail.com>
2022-11-11 00:09:52 +0300

    add table of stations

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import pandas as pd
 
 app = Flask("__name__")
 
+stations_table = pd.read_csv(
+    "data/data_stations/stations.txt",
+    skiprows=17,
+)
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations_table.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
